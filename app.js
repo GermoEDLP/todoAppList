b64b46fe494d228896f665af86bd0c536ca01faa
German Brassini <german.brassini@gmail.com>
2021-03-17 16:43:18 -0300

    Implementacion del menu iteractivo con Inquirer

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
 
-const { inquirerMenu, pause } = require("./helpers/inquirer");
+const {guardarArchivo} = require("./helpers/guardarArchivo");
+const { inquirerMenu, pause, leerInput } = require("./helpers/inquirer");
+const Tareas = require("./models/tareas");
 
 require("colors");
 
 const main = async () => {
   console.log("Hola mundo");
+  const tareas = new Tareas();
 
   let opt = "";
   do {
     opt = await inquirerMenu();
-    console.log({ opt });
+   
+    switch (opt) {
+        case '1':
+            const desc = await leerInput('Descripción: ');
+            tareas.crearTarea(desc);
+            break;
+
+
+        case '2':
+            console.log(tareas.listarTareas);
+            break;
+    
+        default:
+            break;
+    }
+
+    guardarArchivo(tareas.listarTareas)
 
     await pause();
 
